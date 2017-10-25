96389be8ac64018bd88b3e02a0b7c3b39f7abf53
Heather Shapiro <hshapiro93@gmail.com>
2017-10-25 15:26:19 -0400

    added promise for luisintent

--- transformers/luisIntent.js
+++ transformers/luisIntent.js
@@ -2,14 +2,10 @@ function luisIntent (session, messages, configObject) {
   if (typeof configObject.luisRecognizer === 'undefined') {
     return null
   }
-
-  configObject.luisRecognizer.onRecognize(session.toRecognizeContext(), function (err, result) {
-    if (err) {
-      return err
-    }
-
-    // returns undefined. Async issue..
-    return result.intent
+  return new Promise(resolve=>{
+    configObject.luisRecognizer.onRecognize(session.toRecognizeContext(), function (result) {
+      resolve(result.intent)
+    }) 
   })
 }
 
