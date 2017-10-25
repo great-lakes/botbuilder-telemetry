e2d657af92b7a7dca6b81e84fde88a67903fc81b
Kevin Leung <keleung@microsoft.com>
2017-10-25 17:16:48 -0500

    refactored

--- transformers/luisIntent.js
+++ transformers/luisIntent.js
@@ -2,10 +2,13 @@ function luisIntent (session, messages, configObject) {
   if (typeof configObject.luisRecognizer === 'undefined') {
     return null
   }
-  return new Promise(resolve=>{
-    configObject.luisRecognizer.onRecognize(session.toRecognizeContext(), function (err,result) {
+  return new Promise((resolve, reject) => {
+    configObject.luisRecognizer.onRecognize(session.toRecognizeContext(), function (err, result) {
+      if (err) {
+        reject(err)
+      }
       resolve(result.intent)
-    }) 
+    })
   })
 }
 
