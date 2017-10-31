1599b522447d9b45be554c84dd1467de01ee618e
Hao Luo <howlowck@gmail.com>
2017-10-31 11:04:17 -0500

    added function or promise functionality to data mutation

--- index.js
+++ index.js
@@ -1,73 +1,56 @@
 const botResponseLength = require('./transformers/botResponseLength')
-const qnaQuestionMatch = require('./transformers/qnaQuestionMatch')
+// const qnaQuestionMatch = require('./transformers/qnaQuestionMatch')
 const currentDialog = require('./transformers/currentDialog')
 const dialogStack = require('./transformers/dialogStack')
 const botResponse = require('./transformers/botResponse')
-const luisIntent = require('./transformers/luisIntent')
+// const luisIntent = require('./transformers/luisIntent')
 
-function ApplyTelemetryMiddleware (bot, configObject, dataHandleFunction, dataMutationFunction) {
-  if (typeof dataMutationFunction === 'undefined') {
-    dataMutationFunction = function (data) {
-      return data
+function ApplyTelemetryMiddleware (bot, configObject, dataHandleFunction, dataMutationFuncOrPromise = (data) => data) {
+  const dataMutationPromise = (body, session, messages, configObject) => {
+    if (typeof dataMutationFuncOrPromise.then === 'undefined') {
+      return new Promise((resolve, reject) => {
+        resolve(dataMutationFuncOrPromise(body, session, messages, configObject))
+      })
     }
+    return dataMutationFuncOrPromise(body, session, messages, configObject)
+  }
+
+  const onSend = (session, luisResult, qnaResult) => (messages, cb) => {
+    session.library.send(messages, cb)
+    var body = {
+      botName: messages[0].from.name,
+      userName: session.message.user.name,
+      userMessage: session.message.text,
+      userMessageLength: session.message.text.length,
+      userMessageTimestamp: session.message.timestamp,
+      botResponse: botResponse(session, messages, configObject),
+      botResponseLength: botResponseLength(session, messages, configObject),
+      botResponseTimestamp: new Date().toJSON(),
+      botResponseLatency: (new Date().getTime() - new Date(session.message.timestamp).getTime()),
+      currentDialog: currentDialog(session, messages, configObject),
+      dialogStack: dialogStack(session, messages, configObject),
+      luisIntent: luisResult ? luisResult.intent : null
+      // qnaQuestionMatch: qnaQuestionMatch(session, messages, configObject) // TODO triage
+    }
+
+    // A promise to add/mutate and compute data before sending to endpoint
+    dataMutationPromise(body, session, messages, configObject)
+      .then((body) => {
+        // REQUIRED function: API/Endpoint/DB Connection calls
+        dataHandleFunction(body, session, messages, configObject)
+      })
   }
 
   bot.use({
     botbuilder: function (session, next) {
       if (typeof configObject.luisRecognizer === 'undefined') {
-        session.options.onSend = (messages, cb) => {
-          session.library.send(messages, cb)
-          var body = {
-            botName: messages[0].from.name,
-            userName: session.message.user.name,
-            userMessage: session.message.text,
-            userMessageLength: session.message.text.length,
-            userMessageTimestamp: session.message.timestamp,
-            botResponse: botResponse(session, messages, configObject),
-            botResponseLength: botResponseLength(session, messages, configObject),
-            botResponseTimestamp: new Date().toJSON(),
-            botResponseLatency: (new Date().getTime() - new Date(session.message.timestamp).getTime()),
-            currentDialog: currentDialog(session, messages, configObject),
-            dialogStack: dialogStack(session, messages, configObject),
-            luisIntent: luisIntent(session, messages, configObject),
-            qnaQuestionMatch: qnaQuestionMatch(session, messages, configObject) // TODO triage
-          }
-
-          // OPTIONAL function: add/mutate and compute data before sending to endpoint
-          body = dataMutationFunction(body, session, messages, configObject)
-
-          // REQUIRED function: API/Endpoint/DB Connection calls
-          dataHandleFunction(body, session, messages, configObject)
-        }
+        session.options.onSend = onSend(session, null)
       } else {
         configObject.luisRecognizer.onRecognize(session.toRecognizeContext(), function (err, result) {
           if (err) {
             console.log(err)
           }
-          session.options.onSend = (messages, cb) => {
-            session.library.send(messages, cb)
-            var body = {
-              botName: messages[0].from.name,
-              userName: session.message.user.name,
-              userMessage: session.message.text,
-              userMessageLength: session.message.text.length,
-              userMessageTimestamp: session.message.timestamp,
-              botResponse: botResponse(session, messages, configObject),
-              botResponseLength: botResponseLength(session, messages, configObject),
-              botResponseTimestamp: new Date().toJSON(),
-              botResponseLatency: (new Date().getTime() - new Date(session.message.timestamp).getTime()),
-              currentDialog: currentDialog(session, messages, configObject),
-              dialogStack: dialogStack(session, messages, configObject),
-              luisIntent: result.intent
-              // qnaQuestionMatch: qnaQuestionMatch(session, messages, configObject) // TODO triage
-            }
-
-            // OPTIONAL function: add/mutate and compute data before sending to endpoint
-            body = dataMutationFunction(body, session, messages, configObject)
-
-            // REQUIRED function: API/Endpoint/DB Connection calls
-            dataHandleFunction(body, session, messages, configObject)
-          }
+          session.options.onSend = onSend(session, result)
         })
       }
       next()
